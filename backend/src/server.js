8c87df7e56e403d42abebdbbe12d4b246a531d83
Clay0010 <ahmedayad328@gmail.com>
2025-06-27 17:23:50 +0300

    add dotenv import

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,8 @@ import userRoutes from "./routes/user.routes.js";
 import chatRoutes from "./routes/chat.routes.js";
 import cors from "cors";
 import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -14,23 +16,23 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true, // Allow cookies to be sent with requests
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // Allow cookies to be sent with requests
+  })
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  })
-  
+  });
 }
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
